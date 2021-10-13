0a11c45b3084b6c7ab654f1f6fc3c7f5d6739e1c
Melanie Thompson <melanie@ammonitewealth.com>
2021-10-13 14:29:32 +0100

    labels changed

--- client/src/Pages/EditUser/editUser.jsx
+++ client/src/Pages/EditUser/editUser.jsx
@@ -3,15 +3,11 @@ import { useState, useEffect } from "react";
 import "./editUser.css";
 
 function EditUser() {
-  const [fname, setFname] = useState();
-  const [lname, setLname] = useState();
-  const [role, setRole] = useState();
-  const [age, setAge] = useState();
   const [users, setUsers] = useState([]);
 
   const getAllUsers = () => {
     console.log("GETTING ALL USERS");
-    fetch("http://mongo-express-service:5000")
+    fetch("http://localhost:5000")
       .then((res) => res.text())
       .then((res) => {
         console.log("RES", JSON.parse(res));
@@ -20,90 +16,13 @@ function EditUser() {
       .catch((err) => console.log(err));
   };
 
-  const addUser = () => {
-    console.log("5000");
-    fetch("https://mongo-express-service/adduser", {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        user: {
-          fname: fname,
-          lname: lname,
-          role: role,
-          age: age,
-        },
-      }),
-    })
-      .then((res) => res.text())
-      .then((res) => {
-        console.log("RES");
-      });
-  };
-
-  const handleAmendUserField = (event, field, value) => {
-    if (event !== undefined) {
-      event.preventDefault();
-      switch (field) {
-        case "fname":
-          setFname(value);
-          break;
-        case "lname":
-          setLname(value);
-          break;
-        case "role":
-          setRole(value);
-          break;
-        case "age":
-          setAge(value);
-          break;
-      }
-    }
-  };
-
   useEffect(() => {
     getAllUsers();
   }, [fname, lname, role, age]);
 
   return (
     <div className="App">
-      <h1 data-position="header">Testing Redis Cache</h1>
-
-      <section data-position="add-quote">
-        <h2>Add user</h2>
-        <input
-          type="text"
-          placeholder=""
-          value={fname}
-          onChange={(event) =>
-            handleAmendUserField(event, "fname", event.target.value)
-          }
-        />
-        <input
-          type="text"
-          placeholder=""
-          value={lname}
-          onChange={(event) =>
-            handleAmendUserField(event, "lname", event.target.value)
-          }
-        />
-        <input
-          type="text"
-          placeholder=""
-          value={role}
-          onChange={(event) =>
-            handleAmendUserField(event, "role", event.target.value)
-          }
-        />
-        <input
-          type="text"
-          placeholder=""
-          value={age}
-          onChange={(event) =>
-            handleAmendUserField(event, "age", event.target.value)
-          }
-        />
-        <button onClick={() => addUser()}>Submit</button>
-      </section>
+      <h1 data-position="header">Merr k8s testbed</h1>
 
       <section data-position="quotes">
         <h2>Console</h2>
